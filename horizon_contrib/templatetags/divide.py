c2913c0875661ac0c51e8ec21c6deaad90877609
Michael Kuty <6du1ro.n@gmail.com>
2014-06-05 18:15:23 +0200

    pep8 syntax

--- horizon_contrib/templatetags/divide.py
+++ horizon_contrib/templatetags/divide.py
@@ -3,4 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def divide(value, arg): return int(value) / int(arg)
+def divide(value, arg): 
+	return int(value) / int(arg)
