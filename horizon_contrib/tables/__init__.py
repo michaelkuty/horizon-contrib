563fe12fb779803e3be81abdab91ca5e7a7ec338
Michael Kutý <6du1ro.n@gmail.com>
2015-03-15 00:21:37 +0100

    noqa

--- horizon_contrib/tables/__init__.py
+++ horizon_contrib/tables/__init__.py
@@ -2,3 +2,6 @@
 from horizon_contrib.tables.base import (ModelTable, PaginatedModelTable,
                                          PaginatedTable)
 from horizon_contrib.tables.views import IndexView, PaginatedView
+from horizon_contrib.tables.actions import FilterAction, DeleteAction
+
+CRUD_ACTIONS = (DeleteAction,)
