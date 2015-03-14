8391e7b20363a40a39898e590865ce8f15741e31
Michael Kutý <6du1ro.n@gmail.com>
2015-03-14 11:06:19 +0100

    egg

--- setup.py
+++ setup.py
@@ -11,4 +11,4 @@ except ImportError:
 setuptools.setup(
     setup_requires=['pbr'],
     pbr=True,
-    dependency_links=['git+https://github.com/openstack/horizon.git@stable/juno'])
+    dependency_links=['git+https://github.com/openstack/horizon.git@stable/juno#egg=horizon'])
