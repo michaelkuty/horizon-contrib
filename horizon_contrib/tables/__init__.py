9fa3ee0d673f6b63b57d5232553ba4ab5bebdfd1
Michael Kutý <6du1ro.n@gmail.com>
2015-03-14 11:15:41 +0100

    fix wrong import

--- horizon_contrib/tables/__init__.py
+++ horizon_contrib/tables/__init__.py
@@ -1,5 +1,4 @@
 
 from horizon_contrib.tables.base import (ModelTable, PaginatedModelTable,
                                          PaginatedTable)
-from horizon_contrib.tables.common import LogEntryTable
 from horizon_contrib.tables.views import IndexView, PaginatedView
