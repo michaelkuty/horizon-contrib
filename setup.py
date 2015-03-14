06a03ec4c48941b5b04a30ac582e2e212a727f1d
Michael Kutý <6du1ro.n@gmail.com>
2015-03-14 12:16:55 +0100

    setup tuning

--- setup.py
+++ setup.py
@@ -11,4 +11,7 @@ except ImportError:
 setuptools.setup(
     setup_requires=['pbr'],
     pbr=True,
-    dependency_links=['git+https://github.com/openstack/horizon.git@stable/juno#egg=horizon'])
+    dependency_links=['git+https://github.com/openstack/horizon.git@stable/juno#egg=horizon'],
+    extras_require={
+    'horizon':["git+https://github.com/openstack/horizon.git@stable/juno#egg=horizon"],
+    })
