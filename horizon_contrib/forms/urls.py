545f14d6f1065ea1b717d7f81aaf218a1b93dad7
Michael Kutý <6du1ro.n@gmail.com>
2016-03-04 21:57:00 +0100

    Add update with form url.

--- horizon_contrib/forms/urls.py
+++ horizon_contrib/forms/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^models/(?P<cls_name>[\w\.\-]+)/create/$', CreateView.as_view(), name='create'),
     url(r'^models/(?P<cls_name>[\w\.\-]+)/(?P<form_cls>[\w\.\-]+)/create/$', CreateView.as_view(), name='create_with_form'),
     url(r'^models/(?P<cls_name>[\w\.\-]+)/(?P<id>[\w\.\-]+)/update/$', UpdateView.as_view(), name='update'),
+    url(r'^models/(?P<cls_name>[\w\.\-]+)/(?P<id>[\w\.\-]+)/(?P<form_cls>[\w\.\-]+)/update/$', UpdateView.as_view(), name='update_with_form'),
 )
