11daec5f9dd76a24f4cc023a6217d53066a1793e
Michael Kutý <6du1ro.n@gmail.com>
2016-07-18 19:38:00 +0200

    Use new pbr util.

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import setuptools
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
 try:
-    import multiprocessing  # noqa
+    from pbr import util
 except ImportError:
-    pass
-
-setuptools.setup(
-    setup_requires=['testrepository', 'pbr'],
-    pbr=True)
+    setuptools.setup(
+        setup_requires=['pbr'],
+        pbr=True)
+else:
+    setuptools.setup(
+        **util.cfg_to_args())
